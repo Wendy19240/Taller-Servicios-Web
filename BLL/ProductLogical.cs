cdfc86dbbb267861d296197d57574bdf49b2ceb9
Wendy19240 <82130690+Wendy19240@users.noreply.github.com>
2024-11-29 11:47:25 -0500

    Add files via upload

---
+++ BLL/ProductLogical.cs
@@ -0,0 +1,36 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ProductLogical
+    {
+     public ProductLogical Create(ProductLogical newProduct)
+        {
+            ProductLogical Result = null;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                ProductLogical res =
+                    r.Retrieve<Products>(
+                        p => p.ProductName == newProduct.ProductName);
+                if (res != null)
+                {
+                    Result = r.Create(newProduct);
+                }
+                else
+                {
+                    //podriamos lanzar una execpcion
+                    //para notificae que el produxto ya existe 
+                    //podrimos incluso crear una capa de Excepciones
+                    //personalizadas y consumirla desde otras capas
+                }
+            }
+        }
+    }
+}
+
---
+++ SLC/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SLC
+{
+    public interface IProductService
+    {
+        Products Create(Products product);
+        bool Delete(int id);
+    }
+}
